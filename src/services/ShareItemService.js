444a72a4e790928a4164f0e9c13fad73d854bb17
ziqing0451002 <0451002@nkust.edu.tw>
2021-06-04 22:08:35 +0800

    https api test

--- src/services/ShareItemService.js
+++ src/services/ShareItemService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //0604 deploy test 2
-const USER_REST_API_URL = "http://115.165.204.34:8080/ShareItem";
+const USER_REST_API_URL = "https://115.165.204.34:8080/ShareItem";
 
 class ShareItemService{
 
